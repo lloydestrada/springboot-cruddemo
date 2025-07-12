7ce9360e67c37eee587d7751d189a295b808ec4e
lloydestrada <iamlloydestrada@gmail.com>
2025-07-12 21:08:39 +0800

    spring boot cruddemo

---
+++ src/test/java/com/lloydestrada/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloydestrada.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
