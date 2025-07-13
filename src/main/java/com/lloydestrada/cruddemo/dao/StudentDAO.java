38c0871357756673155bb2133b8ba61083ee497d
lloydestrada <iamlloydestrada@gmail.com>
2025-07-13 20:33:14 +0800

    saving object with JPA

---
+++ src/main/java/com/lloydestrada/cruddemo/dao/StudentDAO.java
@@ -0,0 +1,8 @@
+package com.lloydestrada.cruddemo.dao;
+
+import com.lloydestrada.cruddemo.entity.Student;
+
+public interface StudentDAO {
+
+    void save(Student theStudent);
+}
---
+++ src/main/java/com/lloydestrada/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,28 @@
+package com.lloydestrada.cruddemo.dao;
+
+import com.lloydestrada.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+@Repository //for component scanning
+public class StudentDAOImpl implements StudentDAO{
+
+    //define field
+    private EntityManager  entityManager;
+
+    //inject entity manager using constructor injection
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    //implement save method
+    @Override
+    @Transactional
+    public void save(Student theStudent) {
+        entityManager.persist(theStudent);
+    }
+}
