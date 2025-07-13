e2e6896fe8383f773a8ba34ebe3e8fe7efd91aa2
lloydestrada <iamlloydestrada@gmail.com>
2025-07-13 20:39:40 +0800

    saving object with JPA pt2

--- src/main/java/com/lloydestrada/cruddemo/CruddemoApplication.java
+++ src/main/java/com/lloydestrada/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.lloydestrada.cruddemo;
 
+import com.lloydestrada.cruddemo.dao.StudentDAO;
+import com.lloydestrada.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,23 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args){
-
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
 		//lambda expression
 		return runner ->
-				System.out.println("Hello World");
+				createStudent(studentDAO);
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		//create the student object
+		System.out.println("Creating new student...");
+		Student tempStudent = new Student("Lloyd", "Estrada", "lloydestrada@gmail.com");
+
+		//save the student object
+		System.out.println("Saving student....");
+		studentDAO.save(tempStudent);
+
+		//display id of the saved student
+		System.out.println("Saved Student. Generated id: " + tempStudent.getId());
 	}
 }
